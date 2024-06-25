a37435edebb02c0b655a2d8cc4a02774dc9a0d7d
wzm <wangziming2022@163.com>
2024-06-25 23:17:41 +0800

    fix: 调整轮播图标题长度

--- server/internal/domain/model/carousel.go
+++ server/internal/domain/model/carousel.go
@@ -6,7 +6,7 @@ type Carousel struct {
 	gorm.Model
 	Uid         uint   `gorm:"comment:创建者"`
 	Img         string `gorm:"type:varchar(255);comment:图片链接"`
-	Title       string `gorm:"type:varchar(20);comment:标题"`
+	Title       string `gorm:"type:varchar(30);comment:标题"`
 	Url         string `gorm:"type:varchar(100);comment:指向的链接"`
 	Color       string `gorm:"type:varchar(20);comment:主题色"`
 	Use         bool   `gorm:"comment:是否启用"`
