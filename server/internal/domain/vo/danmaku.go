4cb7b2c9954fd936232e6be9892fed9d1297e448
acgkiss <anime@acgkiss.com>
2025-03-06 13:57:06 +0800

    feat：增加了弹幕列表！

--- server/internal/domain/vo/danmaku.go
+++ server/internal/domain/vo/danmaku.go
@@ -1,13 +1,16 @@
 package vo
 
+import "time"
+
 const (
-	DANMAKU_FIELD = "`id`,`time`,`type`,`color`,`text`"
+    DANMAKU_FIELD = "`id`, `time`, `type`, `color`, `text`, `created_at`"
 )
 
 type DanmakuResp struct {
-	ID    uint    `json:"id"`
-	Time  float32 `json:"time"`
-	Type  int     `json:"type"`
-	Color string  `json:"color"`
-	Text  string  `json:"text"`
+    ID        uint      `json:"id"`
+    Time      float32   `json:"time"`
+    Type      int       `json:"type"`
+    Color     string    `json:"color"`
+    Text      string    `json:"text"`
+    CreatedAt time.Time `json:"createdAt"` 
 }
